a030fcc02dfa92e62c4a16c5589455d1a7a43abf
aranajhonny <jhonnyjosearana@gmail.com>
2018-05-03 15:03:50 -0400

    some changes.

--- client.js
+++ client.js
@@ -2,12 +2,255 @@ const { TOKEN } = process.env;
 
 const URL = 'http://api.membrane.io/graphql';
 
-global.fetch = require('node-fetch').fetch;
-
-import { GraphQLClient } from 'graphql-request';
+import { GraphQLClient } from '@membrane/graphql-request';
 
 export const client = new GraphQLClient(URL, {
   headers: {
     Authorization: `Bearer ${TOKEN}`,
   },
 });
+
+export async function listPrograms() {
+  const query = `
+          query {
+            allPrograms {
+              id
+              name
+              latestVersion {
+                id
+              }
+              runningInstances {
+                id
+                alias
+                programVersion {
+                  id
+                }
+                status
+                environment {
+                  name
+                  value
+                }
+                dependencies {
+                  name
+                  value
+                }
+                endpoints {
+                  id
+                  name
+                  type
+                  url
+                  description
+                }
+              }
+            }
+          }
+  `;
+  const result = await client.request(query);
+  return result.allPrograms;
+}
+
+export async function getProgram(id) {
+  const query = `
+          query {
+            program(id: "${id}") {
+              id
+              name
+              latestVersion {
+                id
+              }
+              runningInstances {
+                id
+                alias
+                programVersion {
+                  id
+                }
+                status
+                environment {
+                  name
+                  value
+                }
+                dependencies {
+                  name
+                  value
+                }
+                endpoints {
+                  id
+                  name
+                  type
+                  url
+                  description
+                }
+              }
+            }
+          }
+  `;
+  const result = await client.request(query);
+  return result.program;
+}
+
+export async function getProgramVersion(id) {
+  const query = `
+          query {
+            programVersion(id: "${id}") {
+                id
+                hash
+                programId
+                schema {
+                  types {
+                    name
+                    description
+                    fields {
+                      name
+                      description
+                      params {
+                        name
+                        type
+                        ofType {
+                          type
+                        }
+                      }
+                      type
+                      ofType {
+                        type
+                      }
+                    }
+                    computedFields {
+                      name
+                      description
+                      params {
+                        name
+                        type
+                        ofType {
+                          type
+                        }
+                      }
+                      type
+                      ofType {
+                        type
+                      }
+                    }
+                    actions {
+                      name
+                      description
+                      params {
+                        name
+                        type
+                        ofType {
+                          type
+                        }
+                      }
+                      type
+                      ofType {
+                        type
+                      }
+                    }
+                    events {
+                      name
+                      description
+                      params {
+                        name
+                        type
+                        ofType {
+                          type
+                        }
+                      }
+                      type
+                      ofType {
+                        type
+                      }
+                    }
+                  }
+                  imports {
+                    id
+                    name
+                    types {
+                      name
+                      description
+                      fields {
+                        name
+                        description
+                        params {
+                          name
+                          type
+                          ofType {
+                            type
+                          }
+                        }
+                        type
+                        ofType {
+                          type
+                        }
+                      }
+                      computedFields {
+                        name
+                        description
+                        params {
+                          name
+                          type
+                          ofType {
+                            type
+                          }
+                        }
+                        type
+                        ofType {
+                          type
+                        }
+                      }
+                      actions {
+                        name
+                        description
+                        params {
+                          name
+                          type
+                          ofType {
+                            type
+                          }
+                        }
+                        type
+                        ofType {
+                          type
+                        }
+                      }
+                      events {
+                        name
+                        description
+                        params {
+                          name
+                          type
+                          ofType {
+                            type
+                          }
+                        }
+                        type
+                        ofType {
+                          type
+                        }
+                      }
+                    }
+                  }
+                }
+                dependencies {
+                  description
+                  name
+                  type
+                }
+                environment {
+                  description
+                  name
+                  type
+                }
+                expressions {
+                  description
+                  name
+                  pattern
+                }
+                files {
+                  path
+                  content
+                }
+            }
+          }
+  `;
+  const result = await client.request(query);
+  return result.programVersion;
+}
--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// TODO: everything, waiting for a graphql-client library
-import { client } from './client';
-import 'cross-fetch/polyfill';
+import * as client from './client';
 
 const { root } = program.refs;
 
@@ -14,19 +12,31 @@ export async function init() {
 export const ProgramCollection = {
   async one({ args }) {},
   async items() {
-    const query = `
-    query {
-      allPrograms {
-        id
-        name
-      }
-    }
-  `;
-    const result = await client.request(query);
-    console.log(result);
+    return client.listPrograms();
   },
 };
 
 export const Program = {
-  async self() {},
+  async self({ source }) {
+    return root.programs.one({ id: source.id });
+  },
+
+  async latestVersion({ source }) {
+    return client.getProgramVersion(source.latestVersion.id);
+  },
+};
+
+export const ProgramVersion = {
+  async program({ source }) {
+    return client.getProgram(source.programId);
+  },
+};
+
+export const ProgramInstance = {
+  self({ source }) {
+    return root.programs.one({ id: source.id });
+  },
+  async programVersion({ source }) {
+    return client.getProgramVersion(source.programVersion.id);
+  },
 };
